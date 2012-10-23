e7dea34d021db0c8c014cc32bcef92cb72ee5ced
luochunhui <wo@luochunhui.com>
2012-10-23 10:31:16 +0800

    Initial Commit

---
+++ lushan/LSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LSAppDelegate.h
+//  lushan
+//
+//  Created by Chunhui Luo on 10/23/12.
+//  Copyright (c) 2012 Chunhui Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lushan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  lushan
+//
+//  Created by Chunhui Luo on 10/23/12.
+//  Copyright (c) 2012 Chunhui Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LSAppDelegate class]));
+    }
+}
